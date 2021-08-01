f9dd5ef43d0d4ee37ee18125e6c9df01f9959536
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-08-01 18:57:00 -0500

    Add project files.

---
+++ TheFamilyDictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TheFamilyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+            myFamily.Add("sister", new Dictionary<string, string>() { { "name", "Krista" }, { "age", "42" } });
+            myFamily.Add("brother", new Dictionary<string, string>() { { "name", "Joeffry" }, { "age", "30" } });
+            myFamily.Add("uncle", new Dictionary<string, string>() { { "name", "Paulo" }, { "age", "55" } });
+            myFamily.Add("mother", new Dictionary<string, string>() { { "name", "Roxie" }, { "age", "57" } });
+
+            foreach(KeyValuePair<string> entry in myFamily)
+            {
+
+            }
+        }
+    }
+}
